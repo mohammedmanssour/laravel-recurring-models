16a942e484da34210ba886c2fd3029b06e713024
Mohammed Manssour <manssour.mohammed@gmail.com>
2023-08-22 22:25:02 +0400

    remove loading migrations

--- src/LaravelRecurringModelsServiceProvider.php
+++ src/LaravelRecurringModelsServiceProvider.php
@@ -7,22 +7,12 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class LaravelRecurringModelsServiceProvider extends PackageServiceProvider
 {
-    public function boot()
-    {
-        parent::boot();
-
-        if ($this->app->environment('testing')) {
-            $this->loadMigrationsFrom(__DIR__.'/../tests/Stubs/Migrations');
-        }
-    }
-
     public function configurePackage(Package $package): void
     {
         $package
             ->name('laravel-recurring-models')
             ->hasMigration('1682348400_create_recurring_models_table')
             ->hasMigration('1692297663_add_tz_offset_to_repetitions_table')
-            ->hasMigration('1692434186_adds_week_of_month_to_repetitions_table')
-            ->runsMigrations($this->app->environment('testing'));
+            ->hasMigration('1692434186_adds_week_of_month_to_repetitions_table');
     }
 }
